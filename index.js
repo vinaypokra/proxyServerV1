566ad5a99596cb6e965fb2a660ddb6d0ecc12279
vinaypokra <vvinayppokra@gmail.com>
2023-01-19 11:06:16 +0530

    Update

--- index.js
+++ index.js
@@ -10,47 +10,29 @@ app.use(cors());
 
 
 
-// At request level
-
-
-const error = (err, res) => {
-  const { status = "", statusText = "", data = {} } = err?.response || {};
-  const { error = "", message = "" } = data;
-  console.log("error", err);
-  res.status(status).send(message);
-};
 const arr = [
-  "profile/CompositeProfile/V1/",
-  "billing-account/CompositeBillingAccount/V1/",
-  "service-account/CompositeServiceAccount/V1/",
+  {
+    path: "user",
+    res: {
+      name: "Vinay",
+    },
+  },
+  {
+    path: "users",
+    res: [
+      {
+        name: "Vinay",
+      },
+    ],
+  },
 ];
 
 arr.map((item) => {
-  app.get(`/${item}:name`, async (req, res) => {
-    const name = req.params.name;
-    const agent = new http.Agent({
-        rejectUnauthorized: false,
-      });
-    const config = {
-      method: "get",
-      url: `${url}/${item}${name}`,
-      httpsAgent: agent,
-      headers: {
-        authorization: req.headers.authorization,
-        "access-control-allow-origin": "*",
-        "access-control-allow-headers": "*",
-        Accept: "*/*",
-      },
-    };
-    axios(config)
-      .then(function (response) {
-        res.send(response.data);
-      })
-      .catch(function (err) {
-        error(err, res);
-      });
+  app.get(`/${item.path}`, async (req, res) => {
+    res.send(item.res);
   });
 });
+    
 
 app.listen(3001, () => {
   console.log("Proxy On 3001");
